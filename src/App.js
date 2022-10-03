66181d33e8c6aebd347e1276c38015243519d44c
shreyruhela01 <shreyruhela1311@gmail.com>
2022-10-03 15:11:19 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import FetchUsers from "./components/FetchUsers";
+
+const App = () => {
+  return (
+    < >
+      <h1 className="text-center bg-gray-900 text-white text-4xl pt-5 lg:text-6xl" />
+        Random User Application
+      <FetchUsers />
+    </>
+  );
+}
+
+export default App;
